f619ee6749646783cb7a8da843c0f23a61c52788
Ian Chui <ian.chui@hotmail.com>
2017-11-04 15:13:37 -0400

    all graphs populated with static data

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -6,15 +6,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graph.component.scss']
 })
 export class GraphComponent implements OnInit {
+  state : string;
+  section : number;
 
-  constructor() { }
+  constructor() { 
+    this.section = 0;
+  }
 
   ngOnInit() {
   }
+
+  public chartColors: any[] = [
+    { 
+      backgroundColor:["#FFB6C1", "	#FFF68F", "#90EE90"] 
+    }];
 // Doughnut
-public doughnutChartLabels:string[] = ['<3', '4-6', '>7'];
-public doughnutChartData:number[] = [350, 450, 100];
-public doughnutChartType:string = 'doughnut';
+public oldRateChartLabels:string[] = ['<3', '4-6', '>7'];
+public oldRateChartData:number[] = [500, 250, 100];
+public oldRateChartType:string = 'doughnut';
+
+public newRateChartLabels:string[] = ['<3', '4-6', '>7'];
+public newRateChartData:number[] = [300, 200, 600];
+public newRateChartType:string = 'doughnut';
+
+//Doughnut Location
+public locationChartLabels:string[] = ['Ontario', 'Quebec', 'British Columbia', 'Manitoba','Saskatchewan','Nunavut','P.E.I','New Brunswick'];
+public locationChartData:number[] = [500, 300, 400,60,80,60,150,90];
+public locationChartType:string = 'doughnut';
+
+ // Pie
+ public genderChartLabels:string[] = ['Female', 'Male'];
+ public genderChartData:number[] = [500, 400];
+ public genderChartType:string = 'pie';
+
+  // Pie
+  public ageChartLabels:string[] = ['0-5','6-10', '11-15','15-20'];
+  public ageChartData:number[] = [400, 500,300,150];
+  public ageChartType:string = 'pie';
+
+ // events
+ public chartClicked(e:any):void {
+   console.log(e);
+ }
+
+ public chartHovered(e:any):void {
+   console.log(e);
+ }
+
+public changeSection(x:number){
+  this.section = x;
+  console.log(x);
+}
 
 // events
 public chartClicked(e:any):void {
