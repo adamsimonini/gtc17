de369dc406c9f1d19b66a5855d5d0c18aa37084e
Ian Chui <ian.chui@hotmail.com>
2017-11-04 16:55:39 -0400

    css changes

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -20,29 +20,29 @@ export class GraphComponent implements OnInit {
     {
       backgroundColor: ["#FFB6C1", "	#FFF68F", "#90EE90"]
     }];
-  // Doughnut
-  public oldRateChartLabels: string[] = ['<3', '4-6', '>7'];
-  public oldRateChartData: number[] = [500, 250, 100];
-  public oldRateChartType: string = 'doughnut';
+// Doughnut
+public oldRateChartLabels:string[] = ['0 - 3', '4 - 6', '7 - 10'];
+public oldRateChartData:number[] = [500, 250, 100];
+public oldRateChartType:string = 'doughnut';
 
-  public newRateChartLabels: string[] = ['<3', '4-6', '>7'];
-  public newRateChartData: number[] = [300, 200, 600];
-  public newRateChartType: string = 'doughnut';
+public newRateChartLabels:string[] = ['0 - 3', '4 - 6', '7 - 10'];
+public newRateChartData:number[] = [300, 200, 600];
+public newRateChartType:string = 'doughnut';
 
-  //Doughnut Location
-  public locationChartLabels: string[] = ['Ontario', 'Quebec', 'British Columbia', 'Manitoba', 'Saskatchewan', 'Nunavut', 'P.E.I', 'New Brunswick'];
-  public locationChartData: number[] = [500, 300, 400, 60, 80, 60, 150, 90];
-  public locationChartType: string = 'doughnut';
+//Doughnut Location
+public locationChartLabels:string[] = ['Ontario', 'Quebec', 'British Columbia', 'Manitoba','Saskatchewan','Nunavut'];
+public locationChartData:number[] = [500, 300, 400,60,80,60,150,90];
+public locationChartType:string = 'doughnut';
+
+ // Pie
+ public genderChartLabels:string[] = ['Female', 'Male'];
+ public genderChartData:number[] = [500, 400];
+ public genderChartType:string = 'pie';
 
   // Pie
-  public genderChartLabels: string[] = ['Female', 'Male'];
-  public genderChartData: number[] = [500, 400];
-  public genderChartType: string = 'pie';
-
-  // Pie
-  public ageChartLabels: string[] = ['0-5', '6-10', '11-15', '15-20'];
-  public ageChartData: number[] = [400, 500, 300, 150];
-  public ageChartType: string = 'pie';
+  public ageChartLabels:string[] = ['0 - 5','6 - 10', '11 - 15','15 - 20'];
+  public ageChartData:number[] = [400, 500,300,150];
+  public ageChartType:string = 'pie';
 
   // events
   public chartClicked(e: any): void {
