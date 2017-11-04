b111ddc76e4d6fc972000e7d1523e1ec851788b2
romankhrystynych <roman.khrystynych@gmail.com>
2017-11-04 16:53:05 -0400

    resolved errors

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graph.component.scss']
 })
 export class GraphComponent implements OnInit {
-  state : string;
-  section : number;
+  state: string;
+  section: number;
 
-  constructor() { 
+  constructor() {
     this.section = 0;
   }
 
@@ -17,54 +17,45 @@ export class GraphComponent implements OnInit {
   }
 
   public chartColors: any[] = [
-    { 
-      backgroundColor:["#FFB6C1", "	#FFF68F", "#90EE90"] 
+    {
+      backgroundColor: ["#FFB6C1", "	#FFF68F", "#90EE90"]
     }];
-// Doughnut
-public oldRateChartLabels:string[] = ['<3', '4-6', '>7'];
-public oldRateChartData:number[] = [500, 250, 100];
-public oldRateChartType:string = 'doughnut';
+  // Doughnut
+  public oldRateChartLabels: string[] = ['<3', '4-6', '>7'];
+  public oldRateChartData: number[] = [500, 250, 100];
+  public oldRateChartType: string = 'doughnut';
 
-public newRateChartLabels:string[] = ['<3', '4-6', '>7'];
-public newRateChartData:number[] = [300, 200, 600];
-public newRateChartType:string = 'doughnut';
+  public newRateChartLabels: string[] = ['<3', '4-6', '>7'];
+  public newRateChartData: number[] = [300, 200, 600];
+  public newRateChartType: string = 'doughnut';
 
-//Doughnut Location
-public locationChartLabels:string[] = ['Ontario', 'Quebec', 'British Columbia', 'Manitoba','Saskatchewan','Nunavut','P.E.I','New Brunswick'];
-public locationChartData:number[] = [500, 300, 400,60,80,60,150,90];
-public locationChartType:string = 'doughnut';
-
- // Pie
- public genderChartLabels:string[] = ['Female', 'Male'];
- public genderChartData:number[] = [500, 400];
- public genderChartType:string = 'pie';
+  //Doughnut Location
+  public locationChartLabels: string[] = ['Ontario', 'Quebec', 'British Columbia', 'Manitoba', 'Saskatchewan', 'Nunavut', 'P.E.I', 'New Brunswick'];
+  public locationChartData: number[] = [500, 300, 400, 60, 80, 60, 150, 90];
+  public locationChartType: string = 'doughnut';
 
   // Pie
-  public ageChartLabels:string[] = ['0-5','6-10', '11-15','15-20'];
-  public ageChartData:number[] = [400, 500,300,150];
-  public ageChartType:string = 'pie';
+  public genderChartLabels: string[] = ['Female', 'Male'];
+  public genderChartData: number[] = [500, 400];
+  public genderChartType: string = 'pie';
 
- // events
- public chartClicked(e:any):void {
-   console.log(e);
- }
+  // Pie
+  public ageChartLabels: string[] = ['0-5', '6-10', '11-15', '15-20'];
+  public ageChartData: number[] = [400, 500, 300, 150];
+  public ageChartType: string = 'pie';
 
- public chartHovered(e:any):void {
-   console.log(e);
- }
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
 
-public changeSection(x:number){
-  this.section = x;
-  console.log(x);
-}
+  public changeSection(x: number) {
+    this.section = x;
+    console.log(x);
+  }
 
-// events
-public chartClicked(e:any):void {
-  console.log(e);
-}
-
-public chartHovered(e:any):void {
-  console.log(e);
-}
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 
 }
